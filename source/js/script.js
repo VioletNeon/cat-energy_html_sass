d16dfad54f59e2bad32bc6576b01474d15231e5a
Валерий Куликов <70787286+VioletNeon@users.noreply.github.com>
2021-01-10 21:18:18 +0430

    Доверстать страницу каталога (#13)
    * package-lock.json
    
    * Доверстать главную страницу
    
    * Доверстать страницу формы
    
    * Доверстать страницу каталога
    
    * Доверстать страницу каталога
    
    * Доверстать страницу формы

--- source/js/script.js
+++ source/js/script.js
@@ -4,5 +4,5 @@ var navToggle = document.querySelector('.main-nav__toggle');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
-  navMain.classList.toggle('main-nav--closed');
+  navMain.classList.toggle('main-nav--closed')
 });
